ec89f13dfc64b6c13af44928b6846e75a68f8710
Benjamin Sonntag <benjamin@alternc.org>
2006-05-04 01:53:08 +0000

    quota_edit.sh now computes DATA_PART as quota_get.sh does. Closes #651

--- src/quota_edit.sh
+++ src/quota_edit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 . /etc/alternc/local.sh
-/usr/sbin/setquota -r -g $1 $2 $2 0 0 $ALTERNC_LOC 2>/dev/null || echo "Group quota are not enabled on /var/alternc." >&2
+DATA_PART=`df ${ALTERNC_LOC} 2>/dev/null | awk '/^\// { print $1 }'`
+/usr/sbin/setquota -r -g $1 $2 $2 0 0 $DATA_PART 2>/dev/null || echo "Group quota are not enabled on /var/alternc." >&2
