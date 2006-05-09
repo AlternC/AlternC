3428e1fc5719a6ddae75d7ae44e98d9c54cb96e8
Benjamin Sonntag <benjamin@alternc.org>
2006-05-09 16:12:09 +0000

    remplacement des .po par des .pot + ajout de 'all' comme argument a updatelang.sh

--- bureau/locales/updatelang.sh
+++ bureau/locales/updatelang.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 # Update po files in locales/$1/LC_MESSAGES
 # 
+if [ "$1" = "all" ]
+then
+    find -type d -maxdepth 1 -mindepth 1 -exec ./updatelang.sh {} \;
+    exit 0
+fi
+
 if [ -d $1/LC_MESSAGES ] 
 then
-    for i in *.po 
+    for i in *.pot 
     do
       echo -n "Updating $i : "
-      msgmerge -v -U $1/LC_MESSAGES/$i $i
+      msgmerge -v -U $1/LC_MESSAGES/`echo "$i"|sed -e 's/\.pot$/.po/'` $i
       echo " Done."
     done
 else
-    echo "Directory $1/LC_MESSAGES/ does not exist ! "
+    echo "Usage : updatelang.sh <lang code>"
+    echo "  Update the .po files in <lang code>/LC_MESSAGES directory"
+    echo "  Use 'all' as lang code to update all the po files"
 fi
