ef87340380a82628ef340d0be851d0957c3e65ad
Benjamin Sonntag <benjamin@alternc.org>
2006-05-09 15:58:04 +0000

    changement radical de gestion de la traduction : on utilise maintenant 2 seuls fichiers po pour alternc : main.po qui contient toutes les chaines detectables automatiquement, et main_manual.po qui est alimente manuellement. le dossier modules n'a donc plus lieu d'etre...

--- searchmissing.pot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd bureau
-( find admin -type f ; find class -type f ) | grep -v CVS | grep -v \\.png | sort > /tmp/d1.txt
-cd ../modules
-cat * | sort >/tmp/d2.txt
-diff /tmp/d1.txt /tmp/d2.txt
--- updatepot.sh
+++ updatepot.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #
-# Met a jour les fichiers pot.
+# Met a jour le fichier pot.
 # 
 cd bureau
-for i in `find ../modules -maxdepth 1 -type f \! -name "*~" -printf "%f "`
-  do  
-  xgettext -f../modules/$i -d${i}_tmp -plocales/ -Lphp -k_ -k__ --from-code=iso-8859-1 --msgid-bugs-address="i18n@alternc.org"
-  YEAR=`date +%Y`
-  FULLDATE=`date +"%Y-%m-%d %H:%M %Z"`
-  cat locales/${i}_tmp.po | sed -e "s/# SOME DESCRIPTIVE TITLE./# AlternC's Translation/" | sed -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/$YEAR AlternC's translation team <i18n@alternc.org>/" | sed -e "s/This file is distributed under the same license as the PACKAGE package./This file is distributed under the same license as the AlternC's package./" | sed -e "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./AlternC's translation team <i18n@alternc.org>/" | sed -e 's/Project-Id-Version: PACKAGE VERSION/$id$/' | sed -e "s/PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE/PO-Revision-Date: $FULLDATE/" | sed -e "s/Language-Team: LANGUAGE <LL@li.org>/Language-Team: Team AlternC <i18n@alternc.org>/" | sed -e 's/Content-Type: text\/plain; charset=CHARSET/Content-Type: text\/plain; charset=ISO-8859-1/' | sed -e 's/Last-Translator: FULL NAME <EMAIL@ADDRESS>/Last-Translator: Team AlternC <i18n@alternc.org>/'  >locales/${i}.po
-  rm locales/${i}_tmp.po
-done
+i="main"
+
+xgettext admin/*.php class/*.php -d${i}_tmp -plocales/ -Lphp -k_ -k__ --from-code=iso-8859-1 --msgid-bugs-address="i18n@alternc.org"
+YEAR=`date +%Y`
+FULLDATE=`date +"%Y-%m-%d %H:%M %Z"`
+cat locales/${i}_tmp.po | sed -e "s/# SOME DESCRIPTIVE TITLE./# AlternC's Translation/" | sed -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/$YEAR AlternC's translation team <i18n@alternc.org>/" | sed -e "s/This file is distributed under the same license as the PACKAGE package./This file is distributed under the same license as the AlternC's package./" | sed -e "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./AlternC's translation team <i18n@alternc.org>/" | sed -e 's/Project-Id-Version: PACKAGE VERSION/$id$/' | sed -e "s/PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE/PO-Revision-Date: $FULLDATE/" | sed -e "s/Language-Team: LANGUAGE <LL@li.org>/Language-Team: Team AlternC <i18n@alternc.org>/" | sed -e 's/Content-Type: text\/plain; charset=CHARSET/Content-Type: text\/plain; charset=ISO-8859-1/' | sed -e 's/Last-Translator: FULL NAME <EMAIL@ADDRESS>/Last-Translator: Team AlternC <i18n@alternc.org>/'  >locales/${i}.po
+rm locales/${i}_tmp.po
 
