7a0f1d57c44bd1198f2b74971f322a254ed51901
Antoine Beaupré <anarcat@anarcat.ath.cx>
2007-05-31 17:03:47 +0000

    use absolute path for df
    Closes: #1048

--- src/quota_edit.sh
+++ src/quota_edit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . /etc/alternc/local.sh
-DATA_PART=`df ${ALTERNC_LOC} 2>/dev/null | awk '/^\// { print $1 }'`
+DATA_PART=`/bin/df ${ALTERNC_LOC} 2>/dev/null | awk '/^\// { print $1 }'`
 /usr/sbin/setquota -r -g $1 $2 $2 0 0 $DATA_PART 2>/dev/null || echo "Group quota are not enabled on /var/alternc." >&2
