ca6efbe2a6bf5081f40852b176fd4352c58803da
Benjamin Sonntag <benjamin@alternc.org>
2007-08-26 20:06:39 +0000

    correct html tags on adm_deactivate

--- bureau/admin/adm_deactivate.php
+++ bureau/admin/adm_deactivate.php
@@ -1,19 +1,54 @@
 <?php 
+/*
+ $Id: adm_tld.php,v 1.4 2004/11/29 17:27:04 anonymous Exp $
+ ----------------------------------------------------------------------
+ AlternC - Web Hosting System
+ Copyright (C) 2002 by the AlternC Development Team.
+ http://alternc.org/
+ ----------------------------------------------------------------------
+ Based on:
+ Valentin Lacambre's web hosting softwares: http://altern.org/
+ ----------------------------------------------------------------------
+ LICENSE
 
-require_once('../class/config.php');
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License (GPL)
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ To read the license please visit http://www.gnu.org/copyleft/gpl.html
+ ----------------------------------------------------------------------
+ Original Author of file: Benjamin Sonntag
+ Purpose of file: Manage allowed TLD on the server
+ ----------------------------------------------------------------------
+*/
+require_once("../class/config.php");
+
+include("head.php");
+?>
+</head>
+<body>
+<?php
 $uid = $_GET['uid'];
 if (!$uid) {
 	__("Missing uid");
+	echo "</body></html>";
 	exit();
 }
 if (!$admin->enabled || !$admin->checkcreator($uid)) {
         __("This page is restricted to authorized staff");
+	echo "</body></html>";
 	exit();
 }
 
 if (!$r=$admin->get($uid)) {
 	__("User does not exist");
+	echo "</body></html>";
 	exit();
 }
 
@@ -102,3 +137,8 @@ if ($confirmed) {
   print "</pre>";
 }
 $cuid = $old_cuid;
+
+?>
+</body>
+</html>
+
