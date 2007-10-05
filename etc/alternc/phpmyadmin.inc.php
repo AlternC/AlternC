91d8878aef80aaaafe8f70b478eb4ef55abcfeff
Antoine Beaupré <anarcat@anarcat.ath.cx>
2007-10-05 18:53:50 +0000

    remove useless default configs to simplify this config file

--- etc/alternc/phpmyadmin.inc.php
+++ etc/alternc/phpmyadmin.inc.php
@@ -17,19 +17,7 @@
 $i = 1;
 
 $cfg['Servers'][$i]['host']          = 'localhost'; // MySQL hostname or IP address
-$cfg['Servers'][$i]['port']          = '';          // MySQL port - leave blank for default port
-$cfg['Servers'][$i]['socket']        = '';          // Path to the socket - leave blank for default socket
 $cfg['Servers'][$i]['connect_type']  = 'tcp';    // How to connect to MySQL server ('tcp' or 'socket')
-$cfg['Servers'][$i]['extension']     = 'mysql';     // The php MySQL extension to use ('mysql' or 'mysqli')
-$cfg['Servers'][$i]['compress']      = FALSE;       // Use compressed protocol for the MySQL connection
-//                                                    // (requires PHP >= 4.3.0)
-$cfg['Servers'][$i]['controluser']   = '';          // MySQL control user settings
-//                                                    // (this user must have read-only
-$cfg['Servers'][$i]['controlpass']   = '';          // access to the "mysql/user"
-//                                                    // and "mysql/db" tables).
-//                                                    // The controluser is also
-//                                                    // used for all relational
-//                                                    // features (pmadb)
 $cfg['Servers'][$i]['auth_type']     = 'config';    // Authentication method (config, http or cookie based)?
 $cfg['Servers'][$i]['user']          = $_COOKIE["REMOTE_USER"];    ;      // MySQL user
 $cfg['Servers'][$i]['password']      = $_COOKIE["REMOTE_PASSWORD"]; ;          // MySQL password (only needed
@@ -39,21 +27,7 @@ $i++;
 
 // Uncomment to override the default configuration
 $cfg['Servers'][$i]['host']          = 'localhost'; // MySQL hostname or IP address
-$cfg['Servers'][$i]['port']          = '';          // MySQL port - leave blank for default port
-$cfg['Servers'][$i]['socket']        = '';          // Path to the socket - leave blank for default socket
 $cfg['Servers'][$i]['connect_type']  = 'tcp';    // How to connect to MySQL server ('tcp' or 'socket')
-$cfg['Servers'][$i]['compress']      = FALSE;       // Use compressed protocol for the MySQL connection
-//                                                    // (requires PHP >= 4.3.0)
-$cfg['Servers'][$i]['controluser']   = '';          // MySQL control user settings
-//                                                    // (this user must have read-only
-$cfg['Servers'][$i]['controlpass']   = '';          // access to the "mysql/user"
-//                                                    // and "mysql/db" tables).
-//                                                    // The controluser is also
-//                                                    // used for all relational
-//                                                    // features (pmadb)
 $cfg['Servers'][$i]['auth_type']     = 'cookie';    // Authentication method (config, http or cookie based)?
-$cfg['Servers'][$i]['user']          = 'root';   // MySQL user
-$cfg['Servers'][$i]['password']      = '';          // MySQL password (only needed
-//                                                    // with 'config' auth_type)
 
 ?>
