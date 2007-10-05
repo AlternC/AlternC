82dcc64da5c2b275d56d763dc4a35d80834c2b16
Antoine Beaupré <anarcat@anarcat.ath.cx>
2007-10-05 18:31:07 +0000

    switch to tcp connections in pma so that we can support remote mysqls properly

--- etc/alternc/phpmyadmin.inc.php
+++ etc/alternc/phpmyadmin.inc.php
@@ -19,7 +19,7 @@ $i = 1;
 $cfg['Servers'][$i]['host']          = 'localhost'; // MySQL hostname or IP address
 $cfg['Servers'][$i]['port']          = '';          // MySQL port - leave blank for default port
 $cfg['Servers'][$i]['socket']        = '';          // Path to the socket - leave blank for default socket
-$cfg['Servers'][$i]['connect_type']  = 'socket';    // How to connect to MySQL server ('tcp' or 'socket')
+$cfg['Servers'][$i]['connect_type']  = 'tcp';    // How to connect to MySQL server ('tcp' or 'socket')
 $cfg['Servers'][$i]['extension']     = 'mysql';     // The php MySQL extension to use ('mysql' or 'mysqli')
 $cfg['Servers'][$i]['compress']      = FALSE;       // Use compressed protocol for the MySQL connection
 //                                                    // (requires PHP >= 4.3.0)
@@ -41,7 +41,7 @@ $i++;
 $cfg['Servers'][$i]['host']          = 'localhost'; // MySQL hostname or IP address
 $cfg['Servers'][$i]['port']          = '';          // MySQL port - leave blank for default port
 $cfg['Servers'][$i]['socket']        = '';          // Path to the socket - leave blank for default socket
-$cfg['Servers'][$i]['connect_type']  = 'socket';    // How to connect to MySQL server ('tcp' or 'socket')
+$cfg['Servers'][$i]['connect_type']  = 'tcp';    // How to connect to MySQL server ('tcp' or 'socket')
 $cfg['Servers'][$i]['compress']      = FALSE;       // Use compressed protocol for the MySQL connection
 //                                                    // (requires PHP >= 4.3.0)
 $cfg['Servers'][$i]['controluser']   = '';          // MySQL control user settings
