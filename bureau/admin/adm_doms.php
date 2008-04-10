0e29de73eb1e6d2f1aa4eea2a3e4eb99b09a42b2
Antoine Beaupré <anarcat@anarcat.ath.cx>
2008-04-10 20:05:19 +0000

    integrate patch for linking to domains directly in domain listing. Patch from: Sebastien Grenier Closes: #1097

--- bureau/admin/adm_doms.php
+++ bureau/admin/adm_doms.php
@@ -65,7 +65,7 @@ for($i=0;$i<count($c);$i++) {
 <tr class="lst<?php echo $col; ?>">
 <td><a href="adm_domlock.php?domain=<?php echo urlencode($c[$i][domaine]); ?>"><?php 
    if ($c[$i][noerase]) __("Unlock"); else __("Lock");  ?></a></td>
-<td><?php echo $c[$i][domaine]; ?></td>
+<td><a href="http://<?php echo $c[$i][domaine]; ?>" target="_blank"><?php echo $c[$i][domaine]; ?></a></td>
 <td><?php echo $c[$i][login]; ?></td>
 				    <td><?php if ($c[$i][noerase]) {
 			echo "<img src=\"icon/encrypted.png\" width=\"16\" height=\"16\" alt=\""._("Locked Domain")."\" />";
