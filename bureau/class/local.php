131b6a7f5eec330edeef1c0db3ec119edd9a5a98
Antoine Beaupré <anarcat@anarcat.ath.cx>
2008-04-12 21:07:16 +0000

    cosmetic change: better alignment in variable decl.

--- bureau/class/local.php
+++ bureau/class/local.php
@@ -4,9 +4,9 @@
 $L_VERSION="v. @@REPLACED_DURING_BUILD@@";
 
 /* To ease the transition, we define a lookup table for old names */
-$compat = array('DEFAULT_MX' => 'MX',
-                'MYSQL_USER' => 'MYSQL_LOGIN',
-                'MYSQL_PASS' => 'MYSQL_PWD',
+$compat = array('DEFAULT_MX'   => 'MX',
+                'MYSQL_USER'   => 'MYSQL_LOGIN',
+                'MYSQL_PASS'   => 'MYSQL_PWD',
                 'NS1_HOSTNAME' => 'NS1',
                 'NS2_HOSTNAME' => 'NS2');
 
