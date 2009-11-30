98c27aae57aec6c5b36571cd3495600faff40f3a
Benjamin Sonntag <benjamin@alternc.org>
2009-11-30 06:11:27 +0000

    adding changelog + CREATE TABLE for password policy management features

--- install/upgrades/0.9.10.sql
+++ install/upgrades/0.9.10.sql
@@ -1,1 +1,11 @@
 ALTER IGNORE TABLE `membres` ADD COLUMN `notes` TEXT NOT NULL AFTER `type`;
+
+CREATE TABLE IF NOT EXISTS `policy` (
+  `name` varchar(64) NOT NULL,
+  `minsize` tinyint(3) unsigned NOT NULL,
+  `maxsize` tinyint(3) unsigned NOT NULL,
+  `classcount` tinyint(3) unsigned NOT NULL,
+  `allowlogin` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY  (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='The password policies for services';
+
