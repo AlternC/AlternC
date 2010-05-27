5ab1c458f46cf83e84e2011a0cb41fb75d4bf11f
Chantal Bernard-Putz <chantal@alternc.org>
2010-05-27 13:57:26 +0000

    Texte explicatif sauvegarde bdd modif

--- bureau/admin/sql_restore.php
+++ bureau/admin/sql_restore.php
@@ -51,7 +51,7 @@ if (is_array($r)) {
 <h3><?php printf(_("Restore a MySQL backup for database %s"),$r["db"]); ?></h3>
 <?php
 echo "<p>";
-__("Warning: Write the complete path and the name of the file. <br />For example if your backups are in the directory /Backups ,<br />write /Backups/file.sql.gz (where file.sql.gz is the name of the file)");
+__("Warning: Write the complete path and the name of the file. <br />For example if your backups are in the directory /Backups,<br />write /Backups/file.sql.gz (where file.sql.gz is the name of the file).");
 echo "</p>";
 ?>
 <form action="sql_dorestore.php" method="post">
