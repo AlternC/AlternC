a1837727a549b37deb06f9fdbc157bb134d42668
Camille Lafitte <cam.lafit@azerttyu.net>
2010-10-11 09:35:07 +0000

    La documentation de sqlbackup.sh dit -t  * Le type de backup est indiqué par -t daily/weekly  * evitons donc des messages d'erreur inutiles

--- debian/alternc.cron.d
+++ debian/alternc.cron.d
@@ -2,8 +2,8 @@
 0 4 * * *	root	/usr/lib/alternc/rawstat.daily
 
 # Every day at 5am and every week at 4am, make requested SQL backups
-0 5 * * *	www-data	/usr/lib/alternc/sqlbackup.sh daily
-0 4 * * 0	www-data	/usr/lib/alternc/sqlbackup.sh weekly
+0 5 * * *	www-data	/usr/lib/alternc/sqlbackup.sh -t daily
+0 4 * * 0	www-data	/usr/lib/alternc/sqlbackup.sh -t weekly
 
 # Every 5 minutes, spool waiting domain changes
 */5 * * * *	root		/usr/lib/alternc/update_domains.sh
