830a783fa1e40d8087b8b8bc2d9b63bfd392d676
Alan Garcia <a.garcia@nnx.com>
2011-03-11 10:51:47 +0000

    Bug dans la conf apache Menulang caché par default

--- bureau/admin/menu_lang.php
+++ bureau/admin/menu_lang.php
@@ -1,7 +1,7 @@
 <div class="menu-box">
  <div class="menu-title">
-  <img src="/admin/images/lang.png" alt="<?php __("Langues"); ?>" />&nbsp;<?php __("Langues"); ?></div>
- <div class="menu-content" id="menu-lang">
+  <img src="/admin/images/lang.png" alt="<?php __("Langues"); ?>" />&nbsp;<a href="javascript:menulang_show();"><?php __("Langues"); ?></a></div>
+  <div class="menu-content" id="menu-lang">
   <ul>
    <?php foreach($locales as $l) { ?>
     <li><a href="/admin/login.php?setlang=<?php echo $l; ?>" target="_top"><?php __($l); ?></a></li>
@@ -9,4 +9,9 @@
   </ul>
  </div>
 </div>
-
+<script type="text/javascript">
+  $("#menu-lang").hide();
+  function menulang_show() {
+    $("#menu-lang").show();
+  }
+</script>
