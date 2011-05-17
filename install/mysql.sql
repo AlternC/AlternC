9c85bac55b236845b3fa3629ccbc74249127e4a1
Alan Garcia <a.garcia@nnx.com>
2011-05-17 14:33:35 +0000

    My name is Diot, Idiot.

--- install/mysql.sql
+++ install/mysql.sql
@@ -508,3 +508,85 @@ INSERT IGNORE INTO `domaines_type` (name, description, target, entry, compatibil
 ('defmx2', 'Default backup mail server', 'DOMAIN', '%SUB% IN MX 10 @@DEFAULT_SECONDARY_MX@@', 'vhost,url,ip,webmail,ipv6,cname,txt,defmx',true, false, true),
 ('panel', 'AlternC panel access', 'NONE', '%SUB% IN A @@PUBLIC_IP@@', 'vhost,url,ip,webmail,ipv6,cname,txt,mx,mx2',true, false, true)
 ;
+
+
+-- Add function who are not in mysql 5 to be able ton convert ipv6 to decimal (and reverse it)
+DELIMITER //
+CREATE FUNCTION INET_ATON6(n CHAR(39))
+RETURNS DECIMAL(39) UNSIGNED
+DETERMINISTIC
+BEGIN
+    RETURN CAST(CONV(SUBSTRING(n FROM  1 FOR 4), 16, 10) AS DECIMAL(39))
+                       * 5192296858534827628530496329220096 -- 65536 ^ 7
+         + CAST(CONV(SUBSTRING(n FROM  6 FOR 4), 16, 10) AS DECIMAL(39))
+                       *      79228162514264337593543950336 -- 65536 ^ 6
+         + CAST(CONV(SUBSTRING(n FROM 11 FOR 4), 16, 10) AS DECIMAL(39))
+                       *          1208925819614629174706176 -- 65536 ^ 5
+         + CAST(CONV(SUBSTRING(n FROM 16 FOR 4), 16, 10) AS DECIMAL(39)) 
+                       *               18446744073709551616 -- 65536 ^ 4
+         + CAST(CONV(SUBSTRING(n FROM 21 FOR 4), 16, 10) AS DECIMAL(39))
+                       *                    281474976710656 -- 65536 ^ 3
+         + CAST(CONV(SUBSTRING(n FROM 26 FOR 4), 16, 10) AS DECIMAL(39))
+                       *                         4294967296 -- 65536 ^ 2
+         + CAST(CONV(SUBSTRING(n FROM 31 FOR 4), 16, 10) AS DECIMAL(39))
+                       *                              65536 -- 65536 ^ 1
+         + CAST(CONV(SUBSTRING(n FROM 36 FOR 4), 16, 10) AS DECIMAL(39))
+         ;
+END;
+//
+DELIMITER ;
+DELIMITER //
+CREATE FUNCTION INET_NTOA6(n DECIMAL(39) UNSIGNED)
+RETURNS CHAR(39)
+DETERMINISTIC
+BEGIN
+  DECLARE a CHAR(39)             DEFAULT '';
+  DECLARE i INT                  DEFAULT 7;
+  DECLARE q DECIMAL(39) UNSIGNED DEFAULT 0;
+  DECLARE r INT                  DEFAULT 0;
+  WHILE i DO
+    -- DIV doesn't work with nubers > bigint
+    SET q := FLOOR(n / 65536);
+    SET r := n MOD 65536;
+    SET n := q;
+    SET a := CONCAT_WS(':', LPAD(CONV(r, 10, 16), 4, '0'), a);
+
+    SET i := i - 1;
+  END WHILE;
+
+  SET a := TRIM(TRAILING ':' FROM CONCAT_WS(':',
+                                            LPAD(CONV(n, 10, 16), 4, '0'),
+                                            a));
+
+  RETURN a;
+
+END;
+//
+DELIMITER ;
+
+-- New table for the authorised IP
+CREATE TABLE IF NOT EXISTS `authorised_ip` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `uid` int(11) unsigned NOT NULL default '0',
+  `ip` varchar(40) not null,
+  `subnet` integer(3) not null default 32,
+  `infos` varchar(255) not null default '',
+  PRIMARY KEY  (`id`),
+  KEY `uid` (`uid`)
+) ENGINE=MyISAM COMMENT='Table with list of authorised ip and subnet';
+
+-- Who have authorised IP ?
+CREATE TABLE IF NOT EXISTS `authorised_ip_affected` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `authorised_ip_id` int(10) unsigned not null,
+  `protocol` varchar(15) not null,
+  `parameters` varchar(30) default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM COMMENT='Table with list of protocol <-> authorised ip and subnet';
+
+INSERT IGNORE INTO `variable` (`name` ,`value` ,`comment`)
+VALUES (
+'auth_ip_ftp_default_yes', '1',
+'This variable set if you want to allow all IP address to access FTP by default. If the user start to define some IP or subnet in the allow list, only those he defined will be allowed. This variable can take two value : 0 or 1.'
+);
+
