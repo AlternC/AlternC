923e78ca54742ec50dcf67df113175b22713b109
Alan Garcia <a.garcia@nnx.com>
2011-06-04 16:14:39 +0000

    Changement du numéro de version

--- bureau/class/local.php
+++ bureau/class/local.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Read global variables (AlternC configuration) */
-$L_VERSION="v. 1.0~rc2";
+$L_VERSION="v. 1.0";
 
 /* To ease the transition, we define a lookup table for old names */
 $compat = array('DEFAULT_MX'   => 'MX',
