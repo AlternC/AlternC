c1d3a1e401a241b48f7cc0a7b8088642d662aae6
Steven Mondji-Lerider <squidly@nnx.com>
2012-08-21 18:27:38 +0000

    menu_list pour les logs

---
+++ bureau/admin/menu_logs.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ $Id: menu_logs.php
+ ----------------------------------------------------------------------
+ AlternC - Web Hosting System
+ Copyright (C) 2002 by the AlternC Development Team.
+ http://alternc.org/
+ ----------------------------------------------------------------------
+ Based on:
+ Valentin Lacambre's web hosting softwares: http://altern.org/
+ ----------------------------------------------------------------------
+ LICENSE
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License (GPL)
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ To read the license please visit http://www.gnu.org/copyleft/gpl.html
+ ----------------------------------------------------------------------
+ Original Author of file: Lerider Steven
+ Purpose of file:
+ ----------------------------------------------------------------------
+*/
+
+/* ############# LOGS ############# */
+
+?>
+<div class="menu-box">
+<div class="menu-title"><img src="images/help.png" alt="<?php __("Logs"); ?>" width="16" height="16" />&nbsp;<a href="logs_list.php"><?php __("Logs"); ?></a> </div>
+</div>
+
+
+
