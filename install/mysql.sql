f0185462fe72b37eee121eb35b74e363f744f22f
Alexis Lahouze <xals@lahouze.org>
2012-08-25 11:11:53 +0000

    Fixed accents.

--- install/mysql.sql
+++ install/mysql.sql
@@ -25,11 +25,11 @@
 -- ----------------------------------------------------------------------
 -- STRUCTURE DES TABLES D`ALTERNC
 --
--- IMPORTANT: lorsque la structure de ces tables est modifie, le
--- fichier upgrades/<version>.sql doit tre modifi (ou cr!) pour que
--- les installations courantes soient mises  jour. <version> est ici
--- le prochain numro de version d`AlternC. Voir upgrades/README pour
--- plus de dtails.
+-- IMPORTANT: lorsque la structure de ces tables est modifiée, le
+-- fichier upgrades/<version>.sql doit être modifié (ou créé!) pour que
+-- les installations courantes soient mises à jour. <version> est ici
+-- le prochain numéro de version d`AlternC. Voir upgrades/README pour
+-- plus de détails.
 -- ----------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `slaveip` (
@@ -49,25 +49,25 @@ PRIMARY KEY ( `login` )
 --
 -- Structure de la table `browser`
 --
--- Cette table contient les prfrences des utilisateurs dans le gestionnaire de fichiers
+-- Cette table contient les préférences des utilisateurs dans le gestionnaire de fichiers
 
 
 CREATE TABLE IF NOT EXISTS browser (
-  uid int(10) unsigned NOT NULL default '0',		-- Numro de l`utilisateur
+  uid int(10) unsigned NOT NULL default '0',		-- Numéro de l`utilisateur
   editsizex int(10) unsigned NOT NULL default '0',	-- Largeur de la zone d`edition du brouteur
   editsizey int(10) unsigned NOT NULL default '0',	-- Hauteur de la zone d`edition du brouteur
   listmode tinyint(3) unsigned NOT NULL default '0',	-- Mode de listing (1 colonne, 2 colonne, 3 colonne)
   showicons tinyint(4) NOT NULL default '0',		-- Faut-il afficher les icones (1/0)
-  downfmt tinyint(4) NOT NULL default '0',		-- Format de tlchargement (zip/bz2/tgz/tar.Z)
-  createfile tinyint(4) NOT NULL default '0',		-- Que fait-on aprs cration d`un fichier (1/0)
+  downfmt tinyint(4) NOT NULL default '0',		-- Format de téléchargement (zip/bz2/tgz/tar.Z)
+  createfile tinyint(4) NOT NULL default '0',		-- Que fait-on après création d`un fichier (1/0)
   showtype tinyint(4) NOT NULL default '0',		-- Affiche-t-on le type mime ? 
-  editor_font varchar(64) NOT NULL default '',		-- Nom de la police dans l`diteur de fichiers
-  editor_size varchar(8) NOT NULL default '',		-- Taille de la police dans l`diteur de fichiers
-  crff tinyint(4) NOT NULL default '0',			-- mmorise le dernier fichier/dossier cr (pour le bouton radio)
+  editor_font varchar(64) NOT NULL default '',		-- Nom de la police dans l`éditeur de fichiers
+  editor_size varchar(8) NOT NULL default '',		-- Taille de la police dans l`éditeur de fichiers
+  crff tinyint(4) NOT NULL default '0',			-- mémorise le dernier fichier/dossier créé (pour le bouton radio)
   golastdir tinyint(4) NOT NULL default '0',		-- Faut-il aller au dernier dossier ou au dossier racine dans le brouteur ?
-  lastdir varchar(255) NOT NULL default '',		-- Dernier dossier visit.
+  lastdir varchar(255) NOT NULL default '',		-- Dernier dossier visité.
   PRIMARY KEY  (uid)
-) TYPE=MyISAM COMMENT='Prfrences du gestionnaire de fichiers';
+) TYPE=MyISAM COMMENT='Préférences du gestionnaire de fichiers';
 
 
 --
@@ -76,9 +76,9 @@ CREATE TABLE IF NOT EXISTS browser (
 -- Cette table contient les demandes de changements de mail pour les membres
 
 CREATE TABLE IF NOT EXISTS chgmail (
-  uid int(10) unsigned NOT NULL default '0',		-- Numro de l`utilisateur
+  uid int(10) unsigned NOT NULL default '0',		-- Numéro de l`utilisateur
   cookie varchar(20) NOT NULL default '',		-- Cookie du mail
-  ckey varchar(6) NOT NULL default '',			-- Cl de vrif
+  ckey varchar(6) NOT NULL default '',			-- Clé de vérif
   mail varchar(128) NOT NULL default '',		-- Nouvel Email
   ts bigint(20) unsigned NOT NULL default '0',		-- Timestamp de la demande 
   PRIMARY KEY  (uid)
@@ -91,21 +91,21 @@ CREATE TABLE IF NOT EXISTS chgmail (
 
 CREATE TABLE IF NOT EXISTS db (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  uid int(10) unsigned NOT NULL default '0',		-- Numro de l`utilisateur
+  uid int(10) unsigned NOT NULL default '0',		-- Numéro de l`utilisateur
   login varchar(16) NOT NULL default '',		-- Nom d`utilisateur mysql
   pass varchar(16) NOT NULL default '',			-- Mot de passe mysql
-  db varchar(64) NOT NULL default '',			-- Base de donnes concerne
+  db varchar(64) NOT NULL default '',			-- Base de données concernée
   bck_mode tinyint(3) unsigned NOT NULL default '0',	-- Mode de backup (0/non 1/Daily 2/Weekly)
-  bck_history tinyint(3) unsigned NOT NULL default '0',	-- Nombre de backup  conserver ?
+  bck_history tinyint(3) unsigned NOT NULL default '0',	-- Nombre de backup à conserver ?
   bck_gzip tinyint(3) unsigned NOT NULL default '0',	-- Faut-il compresser les backups ?
-  bck_dir varchar(255) NOT NULL default '',		-- O stocke-t-on les backups sql ?
+  bck_dir varchar(255) NOT NULL default '',		-- Où stocke-t-on les backups sql ?
   PRIMARY KEY id (id)
 ) TYPE=MyISAM COMMENT='Bases MySQL des membres';
 
 --
 -- Structure de la table `domaines`
 --
--- Liste des domaines heberges
+-- Liste des domaines hébergés
 
 CREATE TABLE IF NOT EXISTS domaines (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -141,7 +141,7 @@ CREATE TABLE IF NOT EXISTS ftpusers (
 --
 -- Structure de la table `local`
 --
--- Champs utilisables par l`hbergeur pour associer des donnes locales aux membres.
+-- Champs utilisables par l`hébergeur pour associer des données locales aux membres.
 
 CREATE TABLE IF NOT EXISTS local (
   uid int(10) unsigned NOT NULL default '0',
@@ -156,18 +156,18 @@ CREATE TABLE IF NOT EXISTS local (
 -- Liste des membres
 
 CREATE TABLE IF NOT EXISTS membres (
-  uid int(10) unsigned NOT NULL auto_increment,		-- Numro du membre (GID)
+  uid int(10) unsigned NOT NULL auto_increment,		-- Numéro du membre (GID)
   login varchar(128) NOT NULL default '',		-- Nom d`utilisateur
   pass varchar(64) NOT NULL default '',			-- Mot de passe
   enabled tinyint(4) NOT NULL default '1',		-- Le compte est-il actif ?
   su tinyint(4) NOT NULL default '0',			-- Le compte est-il super-admin ?
   mail varchar(128) NOT NULL default '',		-- Adresse email du possesseur
-  lastaskpass bigint(20) unsigned default '0',		-- Date de dernire demande du pass par mail
+  lastaskpass bigint(20) unsigned default '0',		-- Date de dernière demande du pass par mail
   show_help tinyint(4) NOT NULL default '1',		-- Faut-il afficher l`aide dans le bureau
   lastlogin datetime NOT NULL default '0000-00-00 00:00:00',	-- Date du dernier login
-  lastfail tinyint(4) NOT NULL default '0',		-- Nombre d`checs depuis le dernier login
+  lastfail tinyint(4) NOT NULL default '0',		-- Nombre d`échecs depuis le dernier login
   lastip varchar(255) NOT NULL default '',		-- Nom DNS du client au dernier login
-  creator int(10) unsigned default '0',			-- Qui a cr le compte (quel uid admin)
+  creator int(10) unsigned default '0',			-- Qui a créé le compte (quel uid admin)
   canpass tinyint(4) default '1',			-- L`utilisateur peut-il changer son pass.
   warnlogin tinyint(4) default '0',			-- TODO L`utilisateur veut-il recevoir un mail quand on se loggue sur son compte ?
   warnfailed tinyint(4) default '0',			-- TODO L`utilisateur veut-il recevoir un mail quand on tente de se logguer sur son compte ?
@@ -187,9 +187,9 @@ CREATE TABLE IF NOT EXISTS membres (
 -- Listes des quotas des membres
 
 CREATE TABLE IF NOT EXISTS quotas (
-  uid int(10) unsigned NOT NULL default '0',		-- Numro GID du membre concern
+  uid int(10) unsigned NOT NULL default '0',		-- Numéro GID du membre concerné
   name varchar(64) NOT NULL default '',			-- Nom du quota
-  total bigint(20) unsigned NOT NULL default '0',	-- Quota total (maximum autoris)
+  total bigint(20) unsigned NOT NULL default '0',	-- Quota total (maximum autorisé)
   PRIMARY KEY  (uid,name)
 ) TYPE=MyISAM COMMENT='Quotas des Membres';
 
@@ -200,7 +200,7 @@ CREATE TABLE IF NOT EXISTS quotas (
 
 CREATE TABLE IF NOT EXISTS sessions (
   sid varchar(32) NOT NULL default '',			-- Cookie de session (md5)
-  uid int(10) unsigned NOT NULL default '0',		-- UID du membre concern
+  uid int(10) unsigned NOT NULL default '0',		-- UID du membre concerné
   ip varchar(40) NOT NULL default '',		-- Adresse IP de la connexion
   ts timestamp(14) NOT NULL
 ) TYPE=MyISAM COMMENT='Session actives sur le bureau';
@@ -295,12 +295,12 @@ CREATE TABLE IF NOT EXISTS `mailman` (
 --
 -- Structure de la table `stats3`
 --
--- Liste des jeux de stat brutes demandes sur le serveur
+-- Liste des jeux de stat brutes demandées sur le serveur
 
 CREATE TABLE IF NOT EXISTS stats2 (
-  id int(10) unsigned NOT NULL auto_increment,	-- Numro du jeu de stat brut
-  mid int(10) unsigned NOT NULL default '0',	-- Numro de l`utilisateur
-  hostname varchar(255) NOT NULL default '',	-- Domaine concern
+  id int(10) unsigned NOT NULL auto_increment,	-- Numéro du jeu de stat brut
+  mid int(10) unsigned NOT NULL default '0',	-- Numéro de l`utilisateur
+  hostname varchar(255) NOT NULL default '',	-- Domaine concerné
   folder varchar(255) NOT NULL default '',	-- Dossier de stockage des logs
   PRIMARY KEY  (id),
   KEY mid (mid)
@@ -310,12 +310,12 @@ CREATE TABLE IF NOT EXISTS stats2 (
 --
 -- Structure de la table `defquotas`
 --
--- Quotas par dfaut pour les services
+-- Quotas par défaut pour les services
 
 CREATE TABLE IF NOT EXISTS defquotas (
   quota varchar(128),				-- Nom du quota
   value bigint(20) unsigned default '0',	-- Valeur du quota
-  type  varchar(128) default 'default',		-- Type de compte associe  ce quota
+  type  varchar(128) default 'default',		-- Type de compte associée à ce quota
   PRIMARY KEY (quota,type)
 ) TYPE=MyISAM;
 
@@ -374,14 +374,14 @@ INSERT IGNORE INTO forbidden_domains VALUES ('yahoo.fr');
 --
 -- Structure de la table `tld`
 --
--- Liste des tld autoriss sur ce serveur : 
+-- Liste des tld autorisés sur ce serveur : 
 
 CREATE TABLE IF NOT EXISTS tld (
   tld varchar(128) NOT NULL default '',		-- lettres du tld (sans le .)
-  mode tinyint(4) NOT NULL default '0',		-- Comment est-il autoris ?
+  mode tinyint(4) NOT NULL default '0',		-- Comment est-il autorisé ?
   PRIMARY KEY  (tld),
   KEY mode (mode)
-) TYPE=MyISAM COMMENT='TLD autoriss et comment sont-ils autoriss ? ';
+) TYPE=MyISAM COMMENT='TLD autorisés et comment sont-ils autorisés ? ';
 
 --
 -- Contenu de la table `tld`
