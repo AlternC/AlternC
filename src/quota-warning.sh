6ba876f10c6dd302ae8c7ba2e587ea9e6472e77e
Benjamin Sonntag <benjamin@alternc.org>
2012-08-25 11:29:36 +0000

    adding quota-warning for dovecot + fixing dovecot conf

---
+++ src/quota-warning.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PERCENT=$1
+DOM="`echo $USER | sed -e 's/.*@//'`"
+FROM="postmaster@$DOM"
+
+msg="From: $FROM
+To: $USER
+Subject: Your email quota is $PERCENT% full
+Content-Type: text/plain; charset=UTF-8
+
+Your mailbox is now $PERCENT% full."
+
+echo -e "$msg" | /usr/sbin/sendmail -f $FROM "$USER"
+
+exit 0
