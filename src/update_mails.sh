332070658337a6c93cb1339f40e669627c1572db
Axel ROGER <axel.roger2@gmail.com>
2012-10-15 09:59:21 +0000

    Quotes added to directory vars (MUST ALWAYS BE IN QUOTES!)

--- src/update_mails.sh
+++ src/update_mails.sh
@@ -55,10 +55,10 @@ mysql_query "SELECT id, quote(replace(path,'!','\\!')) FROM mailbox WHERE mail_a
   # If no dir, DELETE
   # If dir and rm ok, DELETE
   # Other case, do nothing
-  if [ -d $path ] ; then 
-    $ionice rm -rf $path && mysql_query "DELETE FROM mailbox WHERE id=$id AND mail_action='DELETING';"
+  if [ -d "$path" ] ; then 
+    $ionice rm -rf "$path" && mysql_query "DELETE FROM mailbox WHERE id=$id AND mail_action='DELETING';"
     # Do the rm again in case of newly added file during delete. Should not be usefull
-    test -d $path && $ionice rm -rf $path
+    test -d "$path" && $ionice rm -rf $path
   else
     mysql_query "DELETE FROM mailbox WHERE id=$id AND mail_action='DELETING';"
   fi
