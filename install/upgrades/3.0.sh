516f1082c0ec2e78e927ab2a5f6890a68a608cbb
Steven Mondji-Lerider <squidly@nnx.com>
2013-01-30 17:32:58 +0000

    sh builtin echo command doesn't understand -e option: replaced with /bin/echo

--- install/upgrades/3.0.sh
+++ install/upgrades/3.0.sh
@@ -4,7 +4,7 @@
 
 # Add INOTIFY_UPDATE_DOMAIN var to local.sh
 if [ -z "$INOTIFY_UPDATE_DOMAIN" ] ; then
-echo -e '
+/bin/echo -e '
 # File to look at for forced launch of update_domain (use incron)
 INOTIFY_UPDATE_DOMAIN="/var/run/alternc/inotify_update_domain.lock"
 ' >> /etc/alternc/local.sh
