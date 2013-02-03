4aded69a06b69ee0f9aa59cbe56ae4f7ba8e479d
Benjamin Sonntag <benjamin@alternc.org>
2013-02-03 13:22:24 +0000

    Fixing Lintian reported bugs: debhelper-but-no-misc-depends, script-not-executable

--- src/export_account.php
+++ src/export_account.php
@@ -1,92 +1,21 @@
-#!/usr/bin/php -q
+#!/usr/bin/php
 <?php
-/**
-* Script permettant d'exporter la configuration d'un compte Alternc ainsi que les données associé
-* A lancer par exemple avant une suppression de compte.
-* Les seuls arguments necessaires sont l'iud du compte à exporter, ainsi que le repertoire ou le dump sera effectué.
-* l'export ce fait sous l'arborescence <dir>/<login>/date/ ou les dumps de configuration sont déposées.
-* Deux sous dossier sont ensuite crée : html ( dump des fichiers web et de la configuration du compte ) 
-* et un dossier sql contenant toutes les bases de données de l'utilisateur.
-*/
 
+// EXPERIMENTAL : user data export.
+die('Proof of concept');
 
-require("/var/alternc/bureau/class/config_nochk.php");
+include("config.php");
 
-global $L_VERSION;
+sem_release($alternc_sem);
 
-if(!chdir("/var/alternc/bureau")){
-    exit(1);
-}
-if($argc != 3 ){
-    echo "usage : export.php <uid> <directory>\n";
-    exit (1);
-}
+$mem->su($id);
 
-$my_id=$argv[1];
-# TODO here test $my_id is numeric
-if((intval($my_id))==0 || (intval($myid)==1)){
-    echo "bad argument: expecting a 4 digit IUD";
-    exit (1);
-}
+$dom->lock();
 
-$dir=$argv[2];
-echo "\n === Export of account $my_id === to $dir\n\n";
+$hooks->invoke("alternc_export",array("/tmp"));
 
+$dom->unlock();
 
-# Connect in this user
-$admin->enabled=1;
-$mem->su($my_id);
-  
-
-
-        if(!is_dir($dir)){
-            if(!mkdir($dir)){
-                echo "creating dir : ".$dir." failed";     
-                exit(1);
-            }
-        }
-        $dir.=$mem->user["login"]."/";
-        if(!is_dir($dir)){
-            if(!mkdir($dir)){
-                echo "creating dir : ".$dir." failed";     
-                exit(1);
-            }
-        }
-        $timestamp=date("Y:m:d");
-        $dir.=$timestamp."/";
-        if(!is_dir($dir)){
-            if(!mkdir($dir)){
-                echo "creating dir : ".$dir." failed";     
-                exit(1);
-            }
-        }
-
-
-
-
-
-# Get the conf
-$conf_user=Array();
-$conf_user=$export->export_conf();
-
-$file_conf=$dir."dump_conf_".date("H:i:s");
-echo $file_conf;
-$file=fopen($file_conf."dump","ab");
-fputs($file,"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?\> \n");
-
-fputs($file,"<version>:".$L_VERSION."</version>\n");
-fputs($file,"<user>".$mem->user["login"]."</user>\n");
-foreach($conf_user as $string){
-    fputs($file,$string);
-}
-//fputs($file,"</html></body>");
-fclose($file);
-
-
-
-# Get the data
-$export->export_data($dir);
 $mem->unsu();
-exit(0);
 
 ?>
