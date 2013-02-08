9dcc34089d16316419519782d3063cbd6a2a4f56
Benjamin Sonntag <benjamin@alternc.org>
2013-02-08 14:51:09 +0000

    repertoire proteges >

--- bureau/admin/menu_web.php
+++ bureau/admin/menu_web.php
@@ -25,6 +25,9 @@
 ?>
 <div class="menu-box">
 <a href="hta_list.php">
-	<div class="menu-title"><img src="images/password.png" alt="<?php __("Protected folders"); ?>" />&nbsp;<?php __("Protected folders"); ?></div>
+  <div class="menu-title">
+    <img src="images/password.png" alt="<?php __("Protected folders"); ?>" />&nbsp;<?php __("Protected folders"); ?>
+    <img src="/images/menu_moins.png" alt="" style="float:right;" id="menu-adm-img"/>
+  </div>
 </a>
 </div>
