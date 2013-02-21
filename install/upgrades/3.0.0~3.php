d32f2372405cbc799e926006d5707c18c68b57ed
Steven Mondji-Lerider <squidly@nnx.com>
2013-02-21 16:45:59 +0000

    Bugfixing upgrade script 3.0.0~3.php adding the special phpmyadmin user to dbusers table

--- install/upgrades/3.0.0~3.php
+++ install/upgrades/3.0.0~3.php
@@ -7,11 +7,13 @@ if(!function_exists('mysql_connect'))  {
     exit(1);
 }
 
+
 // we don't check our AlternC session
 if(!chdir("/var/alternc/bureau"))
   exit(1);
 require("/var/alternc/bureau/class/config_nochk.php");
 
+$db2=new DB_System();
 // we go super-admin
 $admin->enabled=1;
 
@@ -37,10 +39,13 @@ $db->query("select distinct uid,login,pass from db;");
 //on insere dans dbusers avec enabled = admin
 $query=array();
 while($db->next_record()){
- $query[]="insert into dbusers values('',".$db->f('uid').",'".$db->f('login')."','".$db->f('pass')."',\"ADMIN\");";
+ $db2->query("select id from dbusers where name ='".$db->f('login')."' and password='".$db->f('pass')."';");
+ if($db2->num_rows() ==0 ){
+  $query[]="insert ignore into dbusers values('',".$db->f('uid').",'".$db->f('login')."','".$db->f('pass')."',\"ADMIN\");";
+ }
 }
+
 foreach ($query as $q){
-print_r($q);
 $db->query($q);
 }
 
