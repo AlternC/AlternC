216fc12222b3975770576a8d43b0e23730e8ef2b
Steven Mondji-Lerider <squidly@nnx.com>
2013-02-25 12:39:10 +0000

    adding a second script in stable branch for adding phpmyadmin user for the upgrades that failed to add it.

---
+++ install/upgrades/3.0.1.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+<?php
+
+// We check that mysql php module is loaded 
+if(!function_exists('mysql_connect'))  {
+  if(!dl("mysql.so"))
+    exit(1);
+}
+
+
+// we don't check our AlternC session
+if(!chdir("/var/alternc/bureau"))
+exit(1);
+require("/var/alternc/bureau/class/config_nochk.php");
+
+$db2=new DB_System();
+// we go super-admin
+$admin->enabled=1;
+
+$db->query("select distinct uid,login,pass from db;");
+//on insere dans dbusers avec enabled = admin
+$query=array();
+while($db->next_record()){
+  $db2->query("select id from dbusers where name ='".$db->f('login')."' and password='".$db->f('pass')."';");
+  if($db2->num_rows() ==0 ){
+    $query[]="insert ignore into dbusers values('',".$db->f('uid').",'".$db->f('login')."','".$db->f('pass')."',\"ADMIN\");";
+  }
+}
+
+foreach ($query as $q){
+  $db->query($q);
+}
+
+?>
+//done ? :)
