ad5ee05b8bc7c55eb5f32e8f3fd83109c0e76e94
Steven Mondji-Lerider <squidly@nnx.com>
2013-03-29 13:02:24 +0000

    dding an alias for alterncpanel mails to the admin account mail address fix: 1471

--- install/mysql.sql
+++ install/mysql.sql
@@ -640,15 +640,20 @@ where `address`.`enabled` = 1
 -- Structure de la vue `alias_view`
 --
 
-CREATE OR REPLACE VIEW `alias_view` AS 
+CREATE OR REPLACE VIEW `alias_view` AS
 select concat(`address`.`address`,'@',`domaines`.`domaine`) AS `mail`,
-concat(if(isnull(`mailbox`.`id`),'',concat(concat(`address`.`address`,'@',`domaines`.`domaine`),'\n')),`recipient`.`recipients`) AS `alias` 
+concat(if(isnull(`mailbox`.`id`),'',concat(concat(`address`.`address`,'@',`domaines`.`domaine`),'\n')),`recipient`.`recipients`) AS `alias`
 from (((`recipient` join `address` on((`address`.`id` = `recipient`.`address_id`)))
 left join `mailbox` on((`mailbox`.`address_id` = `address`.`id`)))
 join `domaines` on((`domaines`.`id` = `address`.`domain_id`)))
 where `address`.`enabled` = 1
 union
 select distinct concat(`m`.`login`,'@',`v`.`value`) AS `mail`,
+`m`.`mail` AS `alias`
+from ((`membres` `m` join `variable` `v`) join `domaines` `d`)
+where (`v`.`name` = 'mailname_bounce')
+union
+select distinct concat('alterncpanel','@',`v`.`value`) AS `mail`,
 `m`.`mail` AS `alias`
 from ((`membres` `m` join `variable` `v`) join `domaines` `d`)
 where (`v`.`name` = 'mailname_bounce');
