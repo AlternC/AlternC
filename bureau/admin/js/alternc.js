62019ba9f69d67a39080de342455d60b49fa3acf
Alan Garcia <a.garcia@nnx.com>
2013-04-23 22:04:11 +0000

    Corrige un bug js introduit dans mes corrections HTML

--- bureau/admin/js/alternc.js
+++ bureau/admin/js/alternc.js
@@ -240,10 +240,10 @@ function generate_password(len){
 }
 
 function generate_password_html(id, size, field1, field2) {
-  $("#"+id).html("<input id='inp"+id+"' type='textbox' size=8 readonly='readonly' value='"+generate_password(size)+"' />&nbsp;<a href='javascript:generate_password_html("+id+","+size+",\""+field1+"\",\""+field2+"\");'><img src='/images/refresh.png' alt='Refresh' title='Refresh'/></a>");
+  $("#z"+id).html("<input id='inp"+id+"' type='textbox' size=8 readonly='readonly' value='"+generate_password(size)+"' />&nbsp;<a href='javascript:generate_password_html("+id+","+size+",\""+field1+"\",\""+field2+"\");'><img src='/images/refresh.png' alt='Refresh' title='Refresh'/></a>");
   $("#inp"+id).focus();
   $("#inp"+id).select();
-	if (field1 != "") { $(field1).val( $("#inp"+id).val() ); }
-	if (field2 != "") { $(field2).val( $("#inp"+id).val() ); }
+  if (field1 != "") { $(field1).val( $("#inp"+id).val() ); }
+  if (field2 != "") { $(field2).val( $("#inp"+id).val() ); }
 }
 
