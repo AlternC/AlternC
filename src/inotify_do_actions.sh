b3df1b0b2a61f71ca94fbce5662b447b9acc016f
Steven Mondji-Lerider <squidly@nnx.com>
2013-04-24 14:33:49 +0000

    Configuring use of do_action.php by incron ( same way as update_domains.sh )

---
+++ src/inotify_do_actions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Launch do_actions.php if $INOTIFY_DO_ACTION exist
+
+. /etc/alternc/local.sh
+
+LOGGER="/usr/bin/logger"
+
+if [ -e "$INOTIFY_DO_ACTION" ] ; then
+    if [ -x "$LOGGER" ] ; then
+         $LOGGER -t ALTERNC do_actions
+    fi
+    /usr/lib/alternc/do_actions.php
+fi
