078e4391138edccf9af1702fcc6b53ef0a743aee
Alan Garcia <a.garcia@nnx.com>
2014-01-17 18:03:05 +0000

    Ménage : deprecated javascript & browseforfolder

--- bureau/admin/js/alternc.js
+++ bureau/admin/js/alternc.js
@@ -1,89 +1,6 @@
 var menulist = new Array();
 
-/*
-menulist[0] = "menu-dom";
-menulist[1] = "menu-mail";
-menulist[2] = "menu-ftp";
-menulist[3] = "menu-sql";
-menulist[4] = "menu-adm";
-*/
-
 var menu_opened = "";
-
-/*
-function deploy(menu)
-{
-	for (var i = 0; i < menulist.length; i++)
-	{
-		if (checkObj(menulist[i]))
-		{
-			var div_content = new getObj(menulist[i]);
-			var div_img = new getObj("img-" + menulist[i]);
-
-			div_content.style.display = "none";
-			div_img.obj.src = "/images/plus.png";
-		}
-	}
-
-	if (menu == 0)
-		return;
-
-	var div_content = new getObj(menu);
-	var div_img = new getObj("img-" + menu);
-
-	if (menu_opened == menu)
-	{
-		div_content.style.display = "none";
-		div_img.obj.src = "/images/plus.png";
-		menu_opened = "";
-	}
-	else
-	{
-		div_content.style.display = "block";
-		div_img.obj.src = "/images/minus.png";
-		menu_opened = menu;
-	}
-}
-
-function getObj(name)
-{
-	if (document.getElementById)
-	{
-		this.obj = document.getElementById(name);
-		this.style = document.getElementById(name).style;
-	}
-		else if (document.all)
-	{
-		this.obj = document.all[name];
-		this.style = document.all[name].style;
-	}
-		else if (document.layers)
-	{
-		this.obj = document.layers[name];
-		this.style = document.layers[name];
-	}
-}
-
-function checkObj(name)
-{
-	if (document.getElementById)
-	{
-		 if (document.getElementById(name))
-			 return true;
-	}
-		else if (document.all)
-	{
-		if (document.all[name])
-			return true;
-	}
-		else if (document.layers)
-	{
-		if (document.layers[name])
-			return true;
-	}
-	return false;
-}
-*/
 
 function help(hid) {
 	var top=100; /* (10-screen.height); */
@@ -93,12 +10,6 @@ function help(hid) {
 	window.open('./aide/help.php?hid='+hid,'help','top='+top+',left='+left+',width='+largeur+',height='+hauteur+',scrollbars=yes');
 }
 
-function browseforfolder(caller) {
-		eval("file=document."+caller+".value");
-    w=window.open("browseforfolder.php?caller="+caller+"&file="+file,"browseforfolder","width=300,height=400,scrollbars,left=100,top=100");
-		alert("Please contact you administrator (the method 'browseforfolderdeprecated' is normaly deprecated).");
-}
-
 function CheckAll() {
     chk=document.getElementById('checkall').checked;
     for (var i = 0; i < document.main.elements.length; i++) {
@@ -106,81 +17,6 @@ function CheckAll() {
 	    document.main.elements[i].checked = chk;
 	}
     }
-}
-
-function hide(s) {
-    if (document.all) {
-        if (document.all[s]) {
-            document.all[s].visibility="invisible";
-            eval("document.all."+s+".style.display=\"none\"");
-        }
-    } else {
-        if (document.getElementById(s)) {
-            document.getElementById(s).visibility="invisible";
-            document.getElementById(s).style.display="none";
-        }
-    }
-}
-
-/* Affiche le composant s */
-function show(s,shm) {
-    if (!shm) shm="block";
-    if (document.all) {
-        if (document.all[s]) {
-            document.all[s].visibility="visible";
-            eval("document.all."+s+".style.display=\""+shm+"\"");
-        }
-    } else {
-        if (document.getElementById(s)) {
-            document.getElementById(s).visibility="visible";
-            document.getElementById(s).style.display=shm;
-        }
-    }
-}
-/* Affiche / Cache le composant s */
-function swap(s,shm) {
-    if (document.all) {
-        if (document.all[s]) {
-            if (document.all[s].visibility=="visible") {
-                hide(s);
-            } else {
-                show(s,shm);
-            }
-        }
-    } else {
-        if (document.getElementById(s)) {
-            if (document.getElementById(s).visibility=="visible") {
-                hide(s);
-            } else {
-                show(s,shm);
-            }
-        }
-    }
-}
-
-/**
-* Function check_form_mail_validity
-* is used to check if a given mail is a valid RFC 2822 mail adress and set the according image onto the page.
-* @param : id_elem , id of the mail input box we are checking
-*/
-function check_mail_form_validity(id_elem) {
-  var mail = document.getElementById('rcp-'+id_elem).value;
-  var mail_element = document.getElementById('rcp-'+id_elem);
-  var src = "";
-  var alt = "";
-
-  if (mail != "" ) {
-    if(is_valid_mail(mail_element.value) != true ){
-      src = "images/check_no.png";
-      alt = "KO";
-    } else {
-      src ="images/check_ok.png";
-      alt ="OK";
-    }
-  } 
-
-  document.getElementById('valid-rcp-'+id_elem).src = src;
-  document.getElementById('valid-rcp-'+id_elem).alt = alt;
 }
 
 /*
