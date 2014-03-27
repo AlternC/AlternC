0dbd16bbbd59dae056bffc21083748a1e0936aac
Alan Garcia <a.garcia@nnx.com>
2014-03-27 10:03:48 +0000

    Corrige qques pb d'affichage

--- bureau/admin/adm_var_edit.php
+++ bureau/admin/adm_var_edit.php
@@ -251,7 +251,7 @@ foreach ( $variables->strata_order as $strata) {
         foreach ($allvars['FQDN_CREATOR'] as $ttk => $ttv ) {
           if ( isset($ttv[$var]) && is_array( $ttv[$var])) {
             echo sprintf(_("Overwritted by %s"), $members[$ttk]['login'])." &rarr; ";
-            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; echo $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
+            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
             edit_var($ttv[$var]);
           }
           echo "<br/>";
@@ -267,7 +267,7 @@ foreach ( $variables->strata_order as $strata) {
         foreach ($allvars['FQDN'] as $ttk => $ttv ) {
           if ( isset($ttv[$var]) && is_array( $ttv[$var])) {
             echo sprintf(_("Overwritted by %s"), $panel_url[$ttk])." &rarr; ";
-            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; echo $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
+            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
             edit_var($ttv[$var]);
           }
           echo "<br/>";
@@ -283,7 +283,7 @@ foreach ( $variables->strata_order as $strata) {
         foreach ($allvars['CREATOR'] as $ttk => $ttv ) {
           if ( isset($ttv[$var]) && is_array( $ttv[$var])) {
             echo sprintf(_("Overwritted by %s"), $members[$ttk]['login'])." &rarr; ";
-            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; echo $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
+            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
             edit_var($ttv[$var]);
           }
           echo "<br/>";
@@ -299,7 +299,7 @@ foreach ( $variables->strata_order as $strata) {
         foreach ($allvars['MEMBER'] as $ttk => $ttv ) {
           if ( isset($ttv[$var]) && is_array( $ttv[$var])) {
             echo sprintf(_("Overwritted by %s"), $members[$ttk]['login'])." &rarr; ";
-            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; echo $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
+            echo "<a href='javascript:edit_var(".$ttv[$var]['id'].");'>"; $variables->display_valueraw_html($ttv[$var]['value'], $var);echo "</a>";
             edit_var($ttv[$var]);
           }
           echo "<br/>";
