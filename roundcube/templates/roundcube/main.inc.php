fa3b1a63fa4a99b2bef5ef78424c3b57b3339528
Benjamin Sonntag <benjamin@sonntag.fr>
2017-08-03 16:58:59 +0200

    roundcube is now using %u and %p as login and password for SMTP

--- roundcube/templates/roundcube/main.inc.php
+++ roundcube/templates/roundcube/main.inc.php
@@ -147,11 +147,11 @@ $rcmail_config['smtp_port'] = 25;
 
 // SMTP username (if required) if you use %u as the username Roundcube
 // will use the current username for login
-$rcmail_config['smtp_user'] = '';
+$rcmail_config['smtp_user'] = '%u';
 
 // SMTP password (if required) if you use %p as the password Roundcube
 // will use the current user's password for login
-$rcmail_config['smtp_pass'] = '';
+$rcmail_config['smtp_pass'] = '%p';
 
 // SMTP AUTH type (DIGEST-MD5, CRAM-MD5, LOGIN, PLAIN or empty to use
 // best server supported one)
