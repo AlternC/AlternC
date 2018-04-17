88f34571911b109cf2224cfd84ffe413d0d32450
Kienan Stewart <kienan.stewart@burntworld.ca>
2018-04-16 22:46:05 -0400

    Use sha512 crypt to store ftp password hashes

--- install/mysql.sql
+++ install/mysql.sql
@@ -129,7 +129,7 @@ CREATE TABLE IF NOT EXISTS ftpusers (
   id int(10) unsigned NOT NULL auto_increment,
   name varchar(64) NOT NULL default '',
   password varchar(32) NOT NULL default '',
-  encrypted_password VARCHAR(32) default NULL,
+  encrypted_password VARCHAR(255) default NULL,
   homedir varchar(128) NOT NULL default '',
   uid int(10) unsigned NOT NULL default '0',
   enabled boolean NOT NULL DEFAULT TRUE,
