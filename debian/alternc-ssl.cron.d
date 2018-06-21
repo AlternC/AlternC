35250418391433f854f90315511cf3ed1cb1b4d9
cam.lafit <alternc@webelys.com>
2018-06-21 17:05:28 +0200

    No more need to package alternc-ssl
    * Say good bye, thank a lot

--- debian/alternc-ssl.cron.d
+++
@@ -1,5 +0,0 @@
-
-@reboot	  root	      mkdir -p /var/run/alternc-ssl && chown alterncpanel:alterncpanel /var/run/alternc-ssl 
-
-# in case incron didn't work, fix ssl aliases every hour:
-44 * * * * root  /usr/lib/alternc/ssl_alias_manager.sh 
