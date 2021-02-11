f56809fd8ad31ab1bcfeb806573e188dd9a97a18
Nina <nina@koumbit.org>
2021-02-11 16:47:45 -0500

    Append changes instead of overriding
    close #428

--- install/upgrades/3.5.0.4.sql
+++ install/upgrades/3.5.0.4.sql
@@ -1,2 +1,3 @@
 -- Add compatibility between url domain type and MX records. @see GH#428
-UPDATE domaines_type SET compatibility = 'txt,defmx,defmx2,mx,mx2' WHERE name = 'url';
+UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'mx') WHERE name='url' AND compatibility NOT LIKE '%,mx%' AND compatibility NOT LIKE 'mx,%';
+UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'mx2') WHERE name='url' AND compatibility NOT LIKE '%,mx2%' AND compatibility NOT LIKE 'mx2,%';
