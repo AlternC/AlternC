eff34f539c249424f97c4ce0679ba21f4695857b
Nina <nina@koumbit.org>
2023-12-20 00:57:11 -0500

    Add compatibility with dkim domain type
    See https://github.com/AlternC/AlternC/issues/549
    
    * backport PR #576
    * Close #549

--- install/upgrades/3.5.0.5.sql
+++ install/upgrades/3.5.0.5.sql
@@ -1,2 +1,6 @@
 -- Set a default value for the https column in the sub_domaines table
 ALTER TABLE `sub_domaines` MODIFY COLUMN `https` VARCHAR(6) NOT NULL DEFAULT '';
+
+-- Add compatibility with dkim domain type. @see GH#549
+UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'vhost') WHERE name='dkim' AND compatibility NOT LIKE '%vhost%';
+UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'dkim') WHERE name IN (SELECT name FROM domaines_type WHERE compatibility like '%txt%' and name != 'dkim') AND compatibility NOT LIKE '%dkim%';
