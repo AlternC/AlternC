1ba2140f1d793a19962cbf093adc120f37bf46ff
Nina <nina@koumbit.org>
2023-12-26 11:09:35 -0500

    Add DKIM compatibility to any TXT compatible type

--- install/upgrades/3.5.0.5.sql
+++ install/upgrades/3.5.0.5.sql
@@ -1,3 +1,3 @@
 -- Add compatibility with dkim domain type. @see GH#549
 UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'vhost') WHERE name='dkim' AND compatibility NOT LIKE '%vhost%';
-UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'dkim') WHERE name IN ('autodiscover', 'defmx', 'defmx2', 'ip', 'ipv6', 'mx', 'mx2', 'panel', 'txt', 'url', 'vhost', 'vhost-both', 'vhost-http', 'vhost-https') AND compatibility NOT LIKE '%dkim%';
+UPDATE domaines_type SET compatibility=CONCAT_WS(',', compatibility, 'dkim') WHERE name IN (SELECT name FROM domaines_type WHERE compatibility like '%txt%' and name != 'dkim') AND compatibility NOT LIKE '%dkim%';
