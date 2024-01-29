7b73ef8aa01dfc2653ac06ed2e36aaf19011a70a
Camille Lafitte <alternc@webelys.com>
2024-01-29 16:01:43 +0100

    Restore scritp functionnality
    * Get all po file and push them in different subdirectories

--- tx-to-repos.sh
+++ tx-to-repos.sh
@@ -17,6 +17,7 @@
 # and commit everything using svn
 
 hash tx 2>/dev/null || { echo >&2 "tx is required. Retrieve it from https://github.com/transifex/cli . Stopped."; exit 1; }
+hash git 2>/dev/null || { echo >&2 "git is required. Stopped."; exit 1; }
 
 tx pull -a -f
 
@@ -26,37 +27,42 @@ for lang in $langs
 do
     echo "doing lang $lang"
     cp "lang/${lang}.po" "bureau/locales/$lang/LC_MESSAGES/alternc"
-    sublang="`echo $lang | cut -c 1-2`"
+    sublang=$(echo "$lang" | cut -c 1-2)
     # merge the po for debconf into the relevant file for the modules :
     if [ "$lang" != "en_US" ]
     then
-	cat "debian/po/${sublang}.po" | sed -e 's/msgstr ""/msgstr "**DUMMY**"/'  >tmp-debconf.po
-	msgcat --use-first --less-than=3 --more-than=1 -o tmp.po  "lang/${lang}.po" "tmp-debconf.po"
-	rm "tmp-debconf.po"
-	mv -f tmp.po "debian/po/${sublang}.po"
 
-	cat "../alternc-mailman/debian/po/${sublang}.po" | sed -e 's/msgstr ""/msgstr "**DUMMY**"/'  >tmp-debconf.po
-	msgcat --use-first --less-than=3 --more-than=1 -o tmp.po  "lang/${lang}.po" "tmp-debconf.po"
+    tmppo=$(mktemp)
+	sed "debian/po/${sublang}.po" -e 's/msgstr ""/msgstr "**DUMMY**"/' > tmp-debconf.po
+	msgcat --use-first --less-than=3 --more-than=1 -o "$tmppo"  "lang/${lang}.po" "tmp-debconf.po"
 	rm "tmp-debconf.po"
-	mv -f tmp.po "../alternc-mailman/debian/po/${sublang}.po"
+	mv -f "$tmppo" "debian/po/${sublang}.po"
 
-	cat "../alternc-mailman/bureau/locales/$lang/LC_MESSAGES/mailman.po" | sed -e 's/msgstr ""/msgstr "**DUMMY**"/'  >tmp-mailman.po
-	msgcat --use-first --less-than=3 --more-than=1 -o tmp.po  "lang/${lang}.po" "tmp-mailman.po"
+    tmppo=$(mktemp)
+	sed "../alternc-mailman/debian/po/${sublang}.po" -e 's/msgstr ""/msgstr "**DUMMY**"/'  >tmp-debconf.po
+	msgcat --use-first --less-than=3 --more-than=1 -o "$tmppo"  "lang/${lang}.po" "tmp-debconf.po"
+	rm "tmp-debconf.po"
+	mv -f "$tmppo" "../alternc-mailman/debian/po/${sublang}.po"
+
+    tmppo=$(mktemp)
+	sed "../alternc-mailman/bureau/locales/$lang/LC_MESSAGES/mailman.po" -e 's/msgstr ""/msgstr "**DUMMY**"/'  >tmp-mailman.po
+	msgcat --use-first --less-than=3 --more-than=1 -o "$tmppo"  "lang/${lang}.po" "tmp-mailman.po"
 	rm "tmp-mailman.po"
-	mv -f tmp.po "../alternc-mailman/bureau/locales/$lang/LC_MESSAGES/mailman.po"
+	mv -f "$tmppo" "../alternc-mailman/bureau/locales/$lang/LC_MESSAGES/mailman.po"
     fi
     echo "done"
 done
 
 exit 0
 
+# shellcheck disable=SC2317
 if [ "$1" != "nocommit" ]
 then
 # Now committing
     git commit -am "Updating language files from Transifex"
-    pushd ../alternc-mailman
+    pushd ../alternc-mailman || exit
     git commit -am "Updating language files from Transifex"
-    popd
+    popd || exit
 fi
 
 
