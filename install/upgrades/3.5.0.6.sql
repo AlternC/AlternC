ae4e58a5c9876d14b2c87d11c5cce8ed521c0e92
Camille Lafitte <alternc@webelys.com>
2024-07-02 11:22:03 +0200

    Clear duplicate selector alternc rules and prepare them to be updated
    * All upgrade script must be idempotent, then clear actions to provide a stable state

--- install/upgrades/3.5.0.6.sql
+++ install/upgrades/3.5.0.6.sql
@@ -6,3 +6,9 @@ UPDATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHE
 /* Clear invalid selector */
 UPDATE IGNORE sub_domaines SET web_action='DELETE' WHERE sub="@" AND type="dkim" AND enable="ENABLED";
 UPDATE IGNORE sub_domaines SET web_action='DELETE' WHERE type="txt"  AND sub="" AND valeur LIKE "%v=DKIM1%" AND enable="ENABLED";
+
+/* Remove duplicate dkim rules */
+UPDATE sub_domaines SET web_action="DELETE" WHERE sub="alternc._domainkey" AND type="dkim" AND id NOT IN (SELECT MIN(id) FROM sub_domaines WHERE sub="alternc._domainkey" AND type="dkim" GROUP BY domaine);
+
+/* Request a global delete about all action waiting */
+UPDATE domaines SET dns_action='UPDATE' WHERE domaine IN (SELECT domaine FROM sub_domaines WHERE web_action='DELETE');
