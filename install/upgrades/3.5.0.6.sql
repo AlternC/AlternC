d4ac498d3a076030b65d76ff702973ae6201cd63
Camille Lafitte <alternc@webelys.com>
2024-07-02 11:16:46 +0200

    Affect DKIM selector and clear invalid dkim declaration

--- install/upgrades/3.5.0.6.sql
+++ install/upgrades/3.5.0.6.sql
@@ -1,3 +1,8 @@
-UDPATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="dkim" AND sub="@" AND enable="ENABLED";
-UDPATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="dkim" AND sub="" AND enable="ENABLED";
-UDPATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="txt"  AND sub="" AND valeur LIKE "%v=DKIM1%" AND enable="ENABLED";
+/* Affect alternc selector to each DKIM key */
+UPDATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="dkim" AND sub="@" AND enable="ENABLED";
+UPDATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="dkim" AND sub="" AND enable="ENABLED";
+UPDATE IGNORE sub_domaines SET sub='alternc._domainkey', web_action='UPDATE' WHERE type="txt"  AND sub="" AND valeur LIKE "%v=DKIM1%" AND enable="ENABLED";
+
+/* Clear invalid selector */
+UPDATE IGNORE sub_domaines SET web_action='DELETE' WHERE sub="@" AND type="dkim" AND enable="ENABLED";
+UPDATE IGNORE sub_domaines SET web_action='DELETE' WHERE type="txt"  AND sub="" AND valeur LIKE "%v=DKIM1%" AND enable="ENABLED";
